99ae0470fbee51e6458e08273bc0c3ed6ad086c1
clarklindev <clarklindev@gmail.com>
2025-03-10 11:32:41 +0800

    note

--- server/src/lib/socket/chat/actions/connectToDatabase.js
+++ server/src/lib/socket/chat/actions/connectToDatabase.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export async function connectToDatabase(uri, databaseName) {
   console.log("SERVER: STEP 01 - FUNCTION connectToDatabase()");
   try {
+    //NOTE: does not work with VPN
     const result = await mongoose.connect(uri, { dbName: databaseName });
     console.log(`SERVER: connected to mongodb database: ${databaseName}`);
     return result;
